95e4b8661ff2da0cb15fc64a47e56947d7596d31
Bálint Sándor <bsanyi1998@gmail.com>
2021-11-30 20:00:57 +0100

    Refactored parser

---
+++ caffparser/CMakeFiles/caffparser.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sasa/data1/projects/FiukSzGHomework/caffparser/Caff.cpp" "/home/sasa/data1/projects/FiukSzGHomework/caffparser/CMakeFiles/caffparser.dir/Caff.cpp.o"
+  "/home/sasa/data1/projects/FiukSzGHomework/caffparser/CaffParser.cpp" "/home/sasa/data1/projects/FiukSzGHomework/caffparser/CMakeFiles/caffparser.dir/CaffParser.cpp.o"
+  "/home/sasa/data1/projects/FiukSzGHomework/caffparser/Ciff.cpp" "/home/sasa/data1/projects/FiukSzGHomework/caffparser/CMakeFiles/caffparser.dir/Ciff.cpp.o"
+  "/home/sasa/data1/projects/FiukSzGHomework/caffparser/main.cpp" "/home/sasa/data1/projects/FiukSzGHomework/caffparser/CMakeFiles/caffparser.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ caffparser/CMakeFiles/caffparser.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caffparser.dir/Caff.cpp.o"
+  "CMakeFiles/caffparser.dir/CaffParser.cpp.o"
+  "CMakeFiles/caffparser.dir/Ciff.cpp.o"
+  "CMakeFiles/caffparser.dir/main.cpp.o"
+  "caffparser"
+  "caffparser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/caffparser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
