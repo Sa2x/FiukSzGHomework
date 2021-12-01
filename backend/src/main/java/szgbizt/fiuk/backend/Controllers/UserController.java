34fa53775d1cf243f3f80a9295647bc6cbee947e
Garzó Benjamin <beni.garzo@gmail.com>
2021-12-01 23:44:14 +0100

    Fixed editing User and Image Fixed not being able to set name for Image when creating it

--- backend/src/main/java/szgbizt/fiuk/backend/Controllers/UserController.java
+++ backend/src/main/java/szgbizt/fiuk/backend/Controllers/UserController.java
@@ -4,6 +4,9 @@ import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.hibernate.mapping.Any;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ import szgbizt.fiuk.backend.DTOs.TokenDTO;
 import szgbizt.fiuk.backend.Models.User;
 import szgbizt.fiuk.backend.Repositories.UserRepository;
 
+import javax.swing.text.html.Option;
 import java.util.*;
 
 //TODO: Átnézni a kódot, illetve megnézni a visszatérési értékeket, jó-e azok a válaszok, amik most vannak beállítva
@@ -51,9 +55,9 @@ public class UserController {
                 return ResponseEntity.ok(new TokenDTO(jwt));
             }
         }else{
-            return new ResponseEntity("Wrong password!",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(new TokenDTO("Wrong Password!"),HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity("Wrong email!",HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(new TokenDTO("Wrong email!"),HttpStatus.BAD_REQUEST);
     }
 
     //MUKSZIK
@@ -99,9 +103,29 @@ public class UserController {
             return new ResponseEntity("You are not an admin!",HttpStatus.UNAUTHORIZED);
         }
         editableUser.setId(id);
-        userRepository.findById(id).ifPresent(foundUser -> userRepository.save(editableUser));
+        User foundUser = userRepository.findById(id).orElse(null);
+        copyNonNullProperties(editableUser,foundUser);
+        userRepository.save(foundUser);
         return ResponseEntity.ok(userRepository.findById(id).get());
     }
+
+    public static void copyNonNullProperties(Object src, Object target) {
+        BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
+    }
+
+    public static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<String>();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
     //MUKSZIK
     @DeleteMapping(value = "/del/{id}")
     public ResponseEntity<Any> deleteUser(@PathVariable long id,@Auth User user){
